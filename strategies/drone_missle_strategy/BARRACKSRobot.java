437f9fe16f97d341bfad3c584794938844349f4b
Keith Galli <k.galli603@gmail.com>
2015-01-09 23:17:23 -0500

    Fixed something small, pushing

--- strategies/drone_missle_strategy/BARRACKSRobot.java
+++ strategies/drone_missle_strategy/BARRACKSRobot.java
@@ -20,6 +20,7 @@ public class BARRACKSRobot extends BaseRobot {
 	public void run() {
 		try {
 			//
+		    
 
 		} catch (Exception e) {
 			//                    System.out.println("caught exception before it killed us:");
