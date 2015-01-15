321f2d9661649064d0dec3277df638f824e0bb25
Keith Galli <k.galli603@gmail.com>
2015-01-14 20:04:22 -0500

    committing so i can pull

--- strategies/drone_only_strategy/DRONERobot.java
+++ strategies/drone_only_strategy/DRONERobot.java
@@ -48,7 +48,9 @@ public class DRONERobot extends BaseRobot {
 		                  Direction[] directions = getDirectionsToward(this.theirHQ);
 		                  Direction direction = directions[fate];
 		                  RobotPlayer.tryMove(direction);
-		            } else RobotPlayer.tryMove(rc.getLocation().directionTo(this.theirHQ));
+		            } else {
+		                RobotPlayer.tryMove(rc.getLocation().directionTo(this.theirHQ));
+		            }
 		        }
 		    } else {
 		        MapLocation closest  = getClosestTower();
@@ -60,7 +62,7 @@ public class DRONERobot extends BaseRobot {
 
 		    }
 		    
-		    //transferSpecificSupplies(RobotType.DRONE, rc);
+		    transferSpecificSupplies(RobotType.DRONE, rc);
             rc.broadcast(DRONE_CURRENT_CHAN, rc.readBroadcast(DRONE_CURRENT_CHAN)+1);
 		} catch (Exception e) {
 			//                    System.out.println("caught exception before it killed us:");
